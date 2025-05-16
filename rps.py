15a32432889aff52c156f93dc57fd569802c8c5f
Jena Woodburn <jena.woodburn@gmail.com>
2025-05-16 18:50:45 +0200

    Moved game into while loop for continuous play

--- rps.py
+++ rps.py
@@ -1,44 +1,55 @@
 import sys
-import random 
+import random
 from enum import Enum
 
 class RPS(Enum):
+    #define constants
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
 
-# print(RPS(2))
-# print(RPS.ROCK)
-# print(RPS['ROCK'])
-# print(RPS.ROCK.value)
-# sys.exit()
 
+# while loop for continuous play
+play_again = True
 
-print(" ")
+while play_again:
 
-player_choice = input("Enter...\n1 for Rock\n2 for Paper, or\n3 for Scissors\n\n")
+    #get player choice
+    player = int(input("Enter...\n1 for Rock\n2 for Paper, or\n3 for Scissors\n\n"))
 
-player = int(player_choice)
+    #player can only choose from 1, 2 or 3 - if not, exit
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1, 2, or 3")
 
-if player < 1 or player > 3:
-    sys.exit("You must enter 1, 2 or 3")
+    #get computer choice
+    computer = int(random.choice("123"))
 
-computer_choice = random.choice("123")
+    #display player and computer choice
+    print("You chose " + str(RPS(player)).replace('RPS.', '').title())
+    print("Computer chose " + str(RPS(computer)).replace('RPS.', '').title())
+    print("")
 
-computer = int(computer_choice)
+    #logic for determining winner
+    if player == 1 and computer == 3:
+        print("You win 🎉")
+    elif player == 2 and computer == 1:
+        print("You win 🎉")
+    elif player == 3 and computer == 2:
+        print("You win 🎉")
+    elif player == computer:
+        print("It's a tie")
+    else:
+        print("Computer wins") 
+    print("")
 
-print(" ")
-print("You chose " + str(RPS(player)).replace('RPS.', ''))
-print("The computer chose " +  str(RPS(computer)).replace('RPS.', ''))
-print(" ")
+    #ask player to play again or quit
+    play_again = input("Play again?\nY for Yes or \nQ to Quit \n\n")
 
-if player == 1 and computer == 3:
-    print("You win!🎉")
-elif player == 2 and computer == 1:
-    print("You win!🎉")
-elif player == 3 and computer == 2:
-    print("You win!🎉")
-elif player == computer:
-    print("It's a tie!")
-else:
-    print("Computer wins!")
+    if play_again.lower() == 'y':
+        continue
+    else:
+        print("Thanks for playing!")
+        play_again = False
+
+sys.exit()
+
